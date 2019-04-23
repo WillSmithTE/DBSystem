64bb219d9e120ff5143c3b24d9dfc9ddeb058d95
A-G-K <razinagida@gmail.com>
2019-04-24 03:10:07 +1000

    Created Login and Registration Activities

---
+++ DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/RegistrationActivity.java
@@ -0,0 +1,21 @@
+package ses1grp6.dbsystemandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+    /**
+     * Called when the "Sign Up" button is clicked.
+     */
+    public void onSignUpClicked(View view) {
+
+    }
+}
