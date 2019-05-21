32ad3b2291fa1be671442a2d3cfa39590dcb629b
A-G-K <razinagida@gmail.com>
2019-05-21 21:56:55 +1000

    added history item and holder.

--- DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/charity/CharityHistoryFragment.java
+++ DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/charity/CharityHistoryFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +17,6 @@ import java.util.List;
 import ses1grp6.dbsystemandroid.R;
 import ses1grp6.dbsystemandroid.SimpleRecyclerAdaptor;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CharityHistoryFragment extends Fragment implements SimpleRecyclerAdaptor.Binder<CharityHistoryFragment.HistoryHolder> {
 
 
@@ -32,28 +30,31 @@ public class CharityHistoryFragment extends Fragment implements SimpleRecyclerAd
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_charity_history, container, false);
         buildRecyclerView(rootView);
-        // Inflate the layout for this fragment
         return rootView;
     }
 
     private void buildRecyclerView(View rootView) {
         // TODO Sample/Test code
         RecyclerView recyclerView = rootView.findViewById(R.id.charityHistoryRecyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         List<String> l = new ArrayList<>();
-        recyclerView.setAdapter(new SimpleRecyclerAdaptor<>(this, R.layout.donor_item, l));
+        recyclerView.setAdapter(new SimpleRecyclerAdaptor<>(this, R.layout.history_item, l));
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryHolder viewHolder, int i) {
-
+        //TODO BIND DATA
     }
 
     public static class HistoryHolder extends SimpleRecyclerAdaptor.ViewHolder {
 
+        public final TextView title, date, description;
+
         public HistoryHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.historyTitle);
+            date = itemView.findViewById(R.id.historyTime);
+            description = itemView.findViewById(R.id.historyDescription);
         }
     }
 }
