76747349ad8e96fcc584f6d160d2208187cbf9d4
GautamMiglani <31459571+GautamMiglani@users.noreply.github.com>
2019-05-28 15:47:58 +1000

    fixing UI donorWizard

---
+++ DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/charity/charityWizard.java
@@ -0,0 +1,15 @@
+package ses1grp6.dbsystemandroid.charity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ses1grp6.dbsystemandroid.R;
+
+public class charityWizard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity_wizard);
+    }
+}
