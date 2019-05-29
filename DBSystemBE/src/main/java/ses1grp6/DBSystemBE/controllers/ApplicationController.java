446438230bc3d08c0fd498ff443723a79bdf1555
Will Smith <willsmithte@gmail.com>
2019-05-29 18:52:07 +1000

    add applicationController

---
+++ DBSystemBE/src/main/java/ses1grp6/DBSystemBE/controllers/ApplicationController.java
@@ -0,0 +1,42 @@
+package ses1grp6.DBSystemBE.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ses1grp6.DBSystemBE.model.Application;
+import ses1grp6.DBSystemBE.model.Response;
+import ses1grp6.DBSystemBE.repositories.ApplicationRepository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+/**
+ * Created by Will Smith on 29/5/19.
+ */
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+
+    @Autowired
+    ApplicationRepository applicationRepository;
+
+    @Transactional
+    @RequestMapping(value = "accept/{id}", method = RequestMethod.GET)
+    public Response acceptApplication(@PathVariable("id") Integer id) {
+        try {
+            Optional<Application> maybeApplication = applicationRepository.findById(id);
+            if (maybeApplication.isPresent()) {
+                Application application = maybeApplication.get();
+                application.setAccepted(1);
+                return Response.success(applicationRepository.save(application));
+            } else {
+                return Response.fail("Application of id " + id + " not found.");
+            }
+        } catch (Exception e) {
+            return Response.fail("Failed to accept application of id " + id + ": " + e.getMessage());
+        }
+    }
+}
