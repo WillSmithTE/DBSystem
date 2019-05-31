a8b065140ec303267278155894006a521a93f5e3
A-G-K <razinagida@gmail.com>
2019-06-01 01:16:12 +1000

    fixed bug in FragBundler with intents.

--- DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/util/FragBundler.java
+++ DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/util/FragBundler.java
@@ -16,7 +16,6 @@ import ses1grp6.dbsystemandroid.R;
 public class FragBundler {
 
     private static final String INTENT_FRAG_NAME = "dynamicFragment";
-    private static final String INTENT_FRAG_MODEL = "dynamicFragmentModel";
     private Intent intent;
 
     public FragBundler(Intent intent) {
@@ -25,11 +24,6 @@ public class FragBundler {
 
     public void putToIntent(Class<? extends Fragment> fragClz) {
         intent.putExtra(INTENT_FRAG_NAME, fragClz.getName());
-    }
-
-    public <M extends Parcelable> void putToIntent(Class<? extends Fragment> fragClz, M model) {
-        putToIntent(fragClz);
-        intent.putExtra(INTENT_FRAG_MODEL, model);
     }
 
     public Class<? extends Fragment> getFragmentClass() {
@@ -44,16 +38,6 @@ public class FragBundler {
 
     public boolean hasFragment() {
         return intent.hasExtra(INTENT_FRAG_NAME);
-    }
-
-    public boolean hasModel() {
-        return intent.hasExtra(INTENT_FRAG_MODEL);
-    }
-
-    @SuppressWarnings("unchecked")
-    public <M extends Parcelable> M getModel() {
-        if (!hasModel()) throw new RuntimeException("Could not find model in intent");
-        return (M) intent.getParcelableExtra(INTENT_FRAG_MODEL);
     }
 
     public Fragment getFragment() {
