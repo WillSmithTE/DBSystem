821594bd38de17cd6b5f8d7224f5e1341e1c708a
mitcmurph <shotgunpyro@gmail.com>
2019-05-31 21:26:15 +1000

    fixed industry to use dropdown spinner

--- DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/charity/CharityWizard.java
+++ DBAndroid/app/src/main/java/ses1grp6/dbsystemandroid/charity/CharityWizard.java
@@ -4,24 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import ses1grp6.dbsystemandroid.R;
+import ses1grp6.dbsystemandroid.model.Listing;
 import ses1grp6.dbsystemandroid.network.DBSystemNetwork;
 import ses1grp6.dbsystemandroid.network.RequestResponse;
 import ses1grp6.dbsystemandroid.util.UserData;
 import ses1grp6.dbsystemandroid.util.simpleResult.ResultData;
 import ses1grp6.dbsystemandroid.util.simpleResult.SimpleResultActivity;
 
-public class CharityWizard extends AppCompatActivity {
+public class CharityWizard extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    ArrayList<String> industries;
+    ArrayList<Integer> indices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        spinner = (Spinner) findViewById(R.id.charwizSpinner);
+        setupSpinner();
+
         setContentView(R.layout.activity_charity_wizard);
     }
 
@@ -32,11 +45,13 @@ public class CharityWizard extends AppCompatActivity {
         //String charwizDateTime = ((EditText) findViewById(R.id.charwizDateTime)).getText().toString();
         String charwizLocation = ((EditText) findViewById(R.id.charwizLocation)).getText().toString();
         //String charwizIndustry = ((EditText) findViewById(R.id.charwizIndustry)).getText().toString();
+        int charwizIndustry = indices.get(((Spinner) findViewById(R.id.charwizSpinner)).getSelectedItemPosition());
         try {
             jsonObject.put("charity", UserData.getInstance().getId());
             jsonObject.put("listingTitle", charwizTitle);
             jsonObject.put("listingDescription", charwizDescription);
             jsonObject.put("location", charwizLocation);
+            jsonObject.put("industry", charwizIndustry);
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
@@ -57,5 +72,51 @@ public class CharityWizard extends AppCompatActivity {
             }
         });
 
+        System.out.println("REQUEST IS LPO: " + jsonObject.toString());
+
     }
+
+    public void setupSpinner(){
+        DBSystemNetwork.sendGetRequest("/listing/industries", new DBSystemNetwork.OnRequestComplete() {
+            @Override
+            public void onRequestCompleted(RequestResponse response) {
+                if (response.isConnectionSuccessful()) {
+                    try {
+                        industries = new ArrayList<>();
+                        indices = new ArrayList<>();
+
+                        for (int i = 0; i < response.getJsonObject().getJSONArray("body").length(); i++) {
+                            String obj = response.getJsonObject().getJSONArray("body").getJSONObject(i).getString("industryName");
+                            Integer index = response.getJsonObject().getJSONArray("body").getJSONObject(i).getInt("industryID");
+                            industries.add(obj);
+                            indices.add(index);
+                        }
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CharityWizard.this,
+                                android.R.layout.simple_spinner_item,industries);
+                        spinner = (Spinner) findViewById(R.id.charwizSpinner);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinner.setAdapter(adapter);
+                        //spinner.setOnItemSelectedListener(this);
+
+                    } catch (JSONException e) {
+                        System.out.println(e);
+                    }
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 }
